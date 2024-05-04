7c17c65acaeba8b2540112668e7812f0584f3636
helfiesp <59867398+helfiesp@users.noreply.github.com>
2024-05-04 08:14:55 +0200

    Update views.py

--- website_app/views.py
+++ website_app/views.py
@@ -1070,12 +1070,19 @@ def create_order(request, customer_instance):
 
 
 def send_order_confirmation(order):
-    subject = 'Order Confirmation'
+    subject = 'Ordrebekreftelse fra Pokelageret - Ordrenummer: {}'.format(order.order_number)
+    items = json.loads(order.items)
+    item_total = 0
+    for item in items:
+        product = models.product.objects.filter(id=item["item_id"]).first()
+        item["title"] = product.title
+        item_total += Decimal(item["sale_price"])
+
     context = {
         'order': order,
-        'items': json.loads(order.items),  # Assuming 'items' is a JSON string
+        'items': items,
         'payment_details': json.loads(order.payment_info),  # Assuming 'payment_info' is a JSON string
-        'item_total': sum(Decimal(item['sale_price']) for item in json.loads(order.items))
+        'item_total': item_total
     }
     message = render_to_string('admin/order_confirmation_email.html', context)
     email_from = settings.DEFAULT_FROM_EMAIL
